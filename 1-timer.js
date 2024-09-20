32e4d4afd7e16768db4f157e92c18c802af2519c
AntonSmishko <94136660+AntonSmishko@users.noreply.github.com>
2024-09-20 08:33:30 +0000

    Deploying to gh-pages from @ AntonSmishko/goit-js-hw-10@38798fe5da64de5527999b8d90e83651f7708534 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as o}from"./assets/vendor-EyZmBGcZ.js";let e=null;const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){e=t[0],console.log(e)}};o("#datetime-picker",r);
+//# sourceMappingURL=1-timer.js.map
