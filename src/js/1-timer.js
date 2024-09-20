0ed2b2d97c1bf09141512ead2c6d37f8e9b66cb9
RooK| <anton.smishko2014@gmail.com>
2024-09-20 15:40:39 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,6 +4,14 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const btnEl = document.querySelector('[data-start]');
+const dayEl = document.querySelector('[data-days]');
+const hourEl = document.querySelector('[data-hours]');
+const minuteEl = document.querySelector('[data-minutes]');
+const secondEl = document.querySelector('[data-seconds]');
+const dateTimePicker = document.querySelector('#datetime-picker');
+
+btnEl.addEventListener('click', btnElHandler);
+let intervalId = null;
 let userSelectedDate = null;
 btnEl.disabled = true;
 
@@ -19,7 +27,7 @@ const options = {
   },
 };
 
-flatpickr('#datetime-picker', options);
+flatpickr(dateTimePicker, options);
 
 function validateDate() {
   if (userSelectedDate < Date.now()) {
@@ -28,4 +36,46 @@ function validateDate() {
   btnEl.disabled = false;
 }
 
-console.dir(btnEl);
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function btnElHandler() {
+  dateTimePicker.disabled = true;
+  btnEl.disabled = true;
+
+  intervalId = setInterval(updateTimeField, 1000);
+  return;
+}
+
+function updateTimeField() {
+  const curentTime = userSelectedDate - new Date();
+
+  if (curentTime <= 0) {
+    dateTimePicker.disabled = false;
+    clearInterval(intervalId);
+    return;
+  }
+
+  const { days, hours, minutes, seconds } = convertMs(curentTime);
+
+  dayEl.textContent = String(days).padStart(2, 0);
+  hourEl.textContent = String(hours).padStart(2, 0);
+  minuteEl.textContent = String(minutes).padStart(2, 0);
+  secondEl.textContent = String(seconds).padStart(2, 0);
+}
